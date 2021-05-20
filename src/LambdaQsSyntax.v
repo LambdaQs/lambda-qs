3235686b6b2b4afc34043c32d6ca2bc7e366a845
Kartik Singhal <ks@cs.uchicago.edu>
2021-05-20 12:59:33 -0500

    Merge the constants as a workaround

--- src/LambdaQsSyntax.v
+++ src/LambdaQsSyntax.v
@@ -2,28 +2,33 @@ Require Export Hybrid.
 Require Import LambdaQsTypes.
 
 Inductive Econ : Set :=
+  (* Expressions, e *)
     qsLET : Econ
   | qsLAM : Econ
   | qsAP  : Econ
   | qsCMD : Econ
   | qsLOC : nat -> Econ
-.
-
-Inductive Ccon : Set :=
-    qsRET    : Ccon
-  | qsBND    : Ccon
-  | qsDCL    : Ccon
-  | qsGATEAP : Ccon
-  | qsCTRLAP : Ccon
+  (* Commands, m *)
+  | qsRET    : Econ
+  | qsBND    : Econ
+  | qsDCL    : Econ
+  | qsGATEAP : nat -> Econ
+  | qsCTRLAP : nat -> nat -> Econ
+  (* Intrinsics, U *)
+  | qsI : Econ
+  | qsX : Econ
+  | qsY : Econ
+  | qsZ : Econ
+  | qsH : Econ
+  | qsS : Econ
+  | qsT : Econ
 .
 
 Definition qexp : Set := expr Econ.
-Definition qcmd : Set := expr Ccon.
 
 (* Pure expressions **********************************************************)
 
 Definition Var : var -> qexp := VAR Econ.
-Definition Bnd : bnd -> qexp := BND Econ.
 (* let(e1; x.e2) *)
 Definition Let (e1:qexp) (f:qexp -> qexp) : qexp :=
   APP (CON qsLET) (APP e1 (lambda (fun x => f x))).
@@ -33,8 +38,7 @@ Definition Lam (f:qexp -> qexp) : qexp := APP (CON qsLAM) (lambda f).
 Definition Ap (e1 e2:qexp) : qexp := APP (APP (CON qsAP) e1) e2.
 
 (* cmd(m) *)
-(* Definition Cmd (m:qcmd) : qexp := APP (CON qsCMD) m. *)
-(* TODO: The term "m" has type "qcmd" while it is expected to have type "expr Econ". *)
+Definition Cmd (m:qexp) : qexp := APP (CON qsCMD) m.
 
 (* qloc[q] *)
 Definition QLoc (q:nat) : qexp := (CON (qsLOC q)).
@@ -42,5 +46,19 @@ Definition QLoc (q:nat) : qexp := (CON (qsLOC q)).
 (* Effectful commands ********************************************************)
 
 (* ret(e) *)
-(* Definition Ret (e:qexp) : qcmd := APP (CON qsRET) e. *)
-(* The term "e" has type "qexp" while it is expected to have type "expr Ccon". *)
+Definition Ret (e:qexp) : qexp := APP (CON qsRET) e.
+
+(* Definition Bnd : bnd -> qexp := BND Econ. *)
+(* bnd(e; x.m) *)
+Definition Bnd (e:qexp) (f:qexp->qexp) : qexp :=
+  APP (CON qsBND) (APP e (lambda (fun x => f x))).
+
+(* dcl(q.m) *)
+Definition Dcl (f:qexp->qexp) : qexp := APP (CON qsDCL) (lambda f).
+
+(* TODO: ensure validity of U *)
+(* gateap[q](U) *)
+Definition GateAp (q:nat) (u:qexp) : qexp := APP (CON (qsGATEAP q)) u.
+
+(* ctrlap[q1, q2](U) *)
+Definition CtrlAp (q1 q2:nat) (u:qexp) : qexp := APP (CON (qsCTRLAP q1 q2)) u.
