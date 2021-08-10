259d99e7650abdcd51bfccbb4d4caddb60c2d2c2
Kartik Singhal <ks@cs.uchicago.edu>
2021-08-10 12:06:25 -0500

    WIP Menhir-based parser for LangE

---
+++ src/main.ml
@@ -0,0 +1,30 @@
+open LangE_ast
+
+module Lexer = LangE_lexer
+module Parser = LangE_parser
+module PP = LangE_parser_pp
+
+let () = Printf.printf "enter lambda calculus terms, in the grammar t ::= x | t t | \\x.t | (t)\n"
+
+let process (line : string) =
+  let linebuf = Lexing.from_string line in
+  try
+    (* Run the parser on this line of input. *)
+    let t = (Parser.exp_start Lexer.token linebuf) in
+    Printf.printf "   "; PPrintEngine.ToChannel.compact stdout (PP.pp_raw_exp t); Printf.printf "\n";
+    Printf.printf "   "; PPrintEngine.ToChannel.compact stdout (PP.pp_exp t); Printf.printf "\n"
+  with
+  | Lexer.Error msg ->
+      Printf.fprintf stdout "%s" msg
+  | Parser.Error ->
+      Printf.fprintf stdout "%s^\nAt offset %d: syntax error.\n" (String.make (Lexing.lexeme_start linebuf) ' ') (Lexing.lexeme_start linebuf)
+
+let rec foo () =
+  let s = read_line () in
+  process s; flush stdout;
+  foo ()
+
+let () = foo ()
+
+
+
